b0e09ef88b6fbf7976f0ee34f8227f6b53ad744a
Jero-Cardona <jerocarhenao031205@gmail.com>
2023-09-25 21:56:14 -0500

    Firts Commit

---
+++ views/index/index.php
@@ -0,0 +1,15 @@
+<?php
+require "../templade/header.php";
+?>
+    <div class="contenedor" >
+    <div class="caja1">
+        <img class="imagen1" src="img/logo.jpeg">
+        <a href="../login.php"><input type="submit" value="iniciar sesion" class="botones1"></a>
+        <a href="../register.php"><input type="submit" value="Registrarse" class="botones1"></a>
+    </div>
+    </div>
+<?php
+require "../templade/footer.php";
+?>
+</body>
+</html>
